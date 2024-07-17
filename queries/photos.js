70064300d5813bafdc137d1d2b145742010837d9
isaac <imisaacgonzalez@gmail.com>
2024-07-17 18:52:36 -0400

    Changes: CRUD update on photos

--- queries/photos.js
+++ queries/photos.js
@@ -16,4 +16,41 @@ const getOnePhoto = async (id) => {
   }
 };
 
+const createPhoto = async (photo) => {
+  try {
+    return await database.one(
+      "INSERT INTO photos (user_id, url) VALUES ($1, $2) RETURNING *",
+      [photo.user_id, photo.url]
+    );
+  } catch (error) {
+    return null;
+  }
+
+}
+
+const updatePhoto = async (id, photo) => {
+  try {
+    return await database.one(
+      "UPDATE photos SET user_id = $1, url = $2 WHERE id = $3 RETURNING *",
+      [photo.user_id, photo.url, id]
+    );
+  } catch (error) {
+    return null;
+  }
+
+}
+
+const deletePhoto = async (id) => {
+  try {
+    return await database.one(
+      "DELETE FROM photos WHERE id = $1 RETURNING *",
+      id
+    );
+  } catch (error) {
+    return null;
+  }
+
+
+}
+
 module.exports = { getAllPhotos, getOnePhoto };
