36d6f9716c2cb3d1b68e3445ce29cb431dca3007
isaac <imisaacgonzalez@gmail.com>
2024-08-03 16:46:14 -0400

    save: Saving changes done to the SEED/SCHEMA + CRUD on photos

--- queries/users.js
+++ queries/users.js
@@ -18,7 +18,8 @@ const getOneUser = async (id) => {
 
 const createUser = async (user) => {
     try {
-        return await db.one('INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING *', [user.username, user.password, user.email]);
+        return await db.one('INSERT INTO users (username, password, email, profileImg ,address, zipcode, rating, customerType, admin, region, province, city, town, spots,dob,firebase_id, joinedDate) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12,$13,$14,$15,$16,$17) RETURNING *', [user.username, user.password, user.email , user.profileImg, user.address, user.zipcode, user.rating, user.customerType, user.admin, user.region, user.province, user.city, user.town, user.spots, user.dob, user.firebase_id, user.joinedDate
+          ]);
     } catch (error) {
         return null;
     }
